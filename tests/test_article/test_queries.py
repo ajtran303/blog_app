02e27e3caa9a17306a49f0fcb1e495c52bd2d7ce
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2025-12-06 16:59:02 -0500

    Add get article by id

--- tests/test_article/test_queries.py
+++ tests/test_article/test_queries.py
@@ -1,5 +1,5 @@
 from blog.models import Article
-from blog.queries import ListArticlesQuery
+from blog.queries import ListArticlesQuery, GetArticleByIDQuery
 
 def test_list_articles():
     Article(
@@ -17,3 +17,16 @@ def test_list_articles():
     query = ListArticlesQuery()
 
     assert len(query.execute()) == 2
+
+def test_get_article_by_id():
+    article = Article(
+        author = "jane@doe.com",
+        title = "New Article",
+        content = "Super extra awesome article"
+    ).save()
+
+    query = GetArticleByIDQuery(
+        id = article.id
+    )
+
+    assert query.execute().id == article.id
