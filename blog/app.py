2e59d60dc9da1448de93cfbf06c025eff7c3190e
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2025-12-07 16:56:02 -0500

    Handle validation errors

--- blog/app.py
+++ blog/app.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify, request
 from blog.commands import CreateArticleCommand
 from blog.queries import GetArticleByIDQuery, ListArticlesQuery
 
+from pydantic import ValidationError
+
 app = Flask(__name__)
 
 @app.route("/create-article/", methods=["POST"])
@@ -26,6 +28,11 @@ def list_articles():
     records = [record.dict() for record in query.execute()]
     return jsonify(records)
 
+@app.errorhandler(ValidationError)
+def handle_validation_exception(error):
+    response = jsonify(error.errors())
+    response.status_code = 400
+    return response
 
 if __name__ == '__main__':
     app.run()
